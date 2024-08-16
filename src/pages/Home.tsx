88057bd02d38751ad02d1378a41ebe9c2dba600e
joe-black-jb <joeblack3601@gmail.com>
2024-08-16 12:27:12 +0900

    タブの切り替えを無効化

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,5 @@
-import { useEffect, useState } from "react";
-import api from "../api/axiosConfig";
-import Button from "../components/Button";
-import Map from "../components/Map";
-import { LatLng, MarkerData, Place, TabElement } from "../types/types";
+import { useState } from "react";
+import { TabElement } from "../types/types";
 import Map2 from "../components/Map2";
 import { Tabs } from "../components/Tabs";
 import SearchMap from "../components/SearchMap";
@@ -25,30 +22,24 @@ const tabs: TabElement[] = [
 const Home = () => {
   const [selectedTab, setSelectedTab] = useState<TabElement>(tabs[0]);
   const isSearchTab = selectedTab.code === "search";
-  console.log("isSearchTab: ", isSearchTab);
 
   const handleClickTab = (
     e: React.MouseEvent<HTMLElement>,
     tab: TabElement
   ) => {
     e.preventDefault();
-    console.log("クリックしました e: ", e);
-    console.log("e.target: ", e?.target);
-    console.log("tab: ", tab);
     setSelectedTab(tab);
   };
 
-  // 両方読み込んでおいて表示、非表示のみ変えればいいかも
-
   return (
     <>
-      <Tabs
+      {/* <Tabs
         tabs={tabs}
         selectedTab={selectedTab}
         handleClickTab={handleClickTab}
-      />
-      {isSearchTab ? <Map2 /> : <SearchMap />}
-      {/* <Map2 /> */}
+      /> */}
+      {/* {isSearchTab ? <Map2 /> : <SearchMap />} */}
+      <SearchMap />
     </>
   );
 };
