04e5a6cad861d65a5c0d55cfb7435ca611c08cdd
joe-black-jb <joeblack3601@gmail.com>
2024-09-08 00:54:50 +0900

    環境変数の設定を修正

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
 import { config } from "./config/config";
 
-const env = process.env.NODE_ENV || "development";
+const env = process.env.REACT_APP_ENV || "development";
 
 let baseUrl = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
+
 if (env === "development") {
-  baseUrl = config.dev.apiBaseUrl;
+  // baseUrl = config.dev.apiBaseUrl;
   // axios.defaults.headers["Access-Control-Allow-Origin"] = "*";
   axios.defaults.headers.post["Content-Type"] =
     "application/json;charset=utf-8";
